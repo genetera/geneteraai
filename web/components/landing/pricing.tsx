05e07962619268f88195e27b82382fa838b516f6
Niyonshuti Shema Adelite <shemani2011@gmail.com>
2024-02-03 16:28:04 +0100

    Landing page contents

--- web/components/landing/pricing.tsx
+++ web/components/landing/pricing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Pricing = () => {
   return (
-    <section className="bg-slate-50 dark:bg-gray-900">
+    <section className="bg-slate-50 dark:bg-gray-900 hidden">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
